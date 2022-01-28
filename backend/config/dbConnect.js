03629043b61dada1648c2383d018e6ea7de12294
msquare-2 <m4mustafa110@gmail.com>
2022-01-28 22:08:26 +0530

    added frontend

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const dbConnect = () => {
-    mongoose.connect('mongodb+srv://msquare-2:n0RwMtBtcSKvooYa@cluster0.na948.mongodb.net/test')
-    .then(()=> console.log('Db Connected'))
+  //connect DB
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log('Db Connected'))
     .catch(err => console.log(err));
-}
+};
 
 module.exports = dbConnect;
-
--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,27 @@
 const express = require('express');
-const dbConnect = require('./config/dbConnect')();
+const dotenv = require('dotenv');
+const error = require('./middlewares/errorMiddlewareHandler');
+const usersRoute = require('./routes/usersRoute');
+const bookRouter = require('./routes/bookRoutes');
+dotenv.config();
+require('./config/dbConnect')();
+
 const app = express();
 
+//Passing body data
+app.use(express.json());
 
-// Routes
+//Routes
+//Users
+app.use('/api/users', usersRoute);
+//Books
+app.use('/api/books', bookRouter);
 
-// User Routes
-// Register
-app.post('/api/users/register', (req, res) => {
-    res.send('Register route');
-});
-// Login
-app.post('/api/users/login', (req, res) => {
-    res.send('Login route');
-});
-// Update User
-app.put('/api/users/update', (req, res) => {
-    res.send('Update route');
-});
-// Delete User
-app.delete('/api/users/:id', (req, res) => {
-    res.send('Delete route');
-});
-// fetch users
-app.put('/api/users', (req, res) => {
-    res.send('Fetch users');
-});
+//Error middleware
+app.use(error.errorMiddlewareHandler);
 
-
-
-// Server
-const PORT =process.env.PORT || 5000;
+//Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
+  console.log(`Server is up and runing ${PORT}`);
 });
-
-//n0RwMtBtcSKvooYa
-//mongodb+srv://msquare-2:<password>@cluster0.na948.mongodb.net/test
